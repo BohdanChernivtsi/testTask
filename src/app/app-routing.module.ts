d55000cd70a03fc112ce4f06f8ac79ffd499b563
Bohdan Onufriychuk <bohdan.onufriichuk@windmill.ch>
2020-07-23 14:35:12 +0300

    added lazy modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PreloadAllModules } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
+  }, {
+    path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule)
+  }, {
+    path: 'table', loadChildren: () => import('./features/table/table.module').then(m => m.TableModule)
+  }, {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {
+      preloadingStrategy: PreloadAllModules
+    }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
